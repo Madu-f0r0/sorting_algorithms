75b76d24ca7dc5f3fb0ee00eb316284e68aa309a
Madu-f0r0 <duherbert@gmail.com>
2023-09-26 21:40:18 +0100

    Created shell sort algorithm

---
+++ 100-shell_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+void shell_sort(int *array, size_t size)
+{
+	int i, j, temp, gap = 1;
+
+	if (array && size)
+	{
+		/* Calculate the starting gap (using Knuth's Sequence) */
+		while (gap < (int)size)
+		{
+			if (gap * 3 + 1 > (int)size)
+				break;
+			gap = gap * 3 + 1;
+		}
+
+		/* Initiate gap interval (using Knuth's Sequence) */
+		for (; gap >= 1; gap = (gap - 1) / 3)
+		{
+			for (j = gap; j < (int)size ;j++)
+			{
+				for (i = j - gap; i >= 0; i -= gap)
+				{
+					if (array[i + gap] < array[i])
+					{
+						temp = array[i];
+						array[i] = array[i + gap];
+						array[i + gap] = temp;
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+			print_array(array, size);
+
+		}
+	}
+}
